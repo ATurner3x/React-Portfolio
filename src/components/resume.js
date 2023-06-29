3d79200e00b5d1333444530d9ca9aa97451f7aff
Amadeus <a.turner3x@gmail.com>
2023-06-29 13:42:35 -0400

    added resume component

---
+++ src/components/resume.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Resume = () => {
+  return (
+    <section>
+      <h2>Resume</h2>
+      {/* Add link to a downloadable resume here */}
+    </section>
+  );
+};
+
+export default Resume;
