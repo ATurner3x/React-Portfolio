0a90aa5ea0c851ee2cd886cad03f69d38de61ff0
Amadeus <a.turner3x@gmail.com>
2023-06-29 13:34:11 -0400

    added the portfolio component

---
+++ src/components/portfolio.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Portfolio = () => {
+  return (
+    <section>
+      <h2>Portfolio</h2>
+      {/* Add titled images of applications with links */}
+    </section>
+  );
+};
+
+export default Portfolio;
