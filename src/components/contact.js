06cdbf1cc81ee3f9d85ff4952f9b5a835f4d57ed
Amadeus <a.turner3x@gmail.com>
2023-06-29 13:40:37 -0400

    added the contact page

---
+++ src/components/contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <section>
+      <h2>Contact</h2>
+      {/* Add a contact form with fields for name, email address, and message */}
+    </section>
+  );
+};
+
+export default Contact;
