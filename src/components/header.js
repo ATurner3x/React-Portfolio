39fac6f14f79db1a49576746ea3dec72111a9a50
Amadeus <a.turner3x@gmail.com>
2023-06-29 13:19:48 -0400

    create header.js

---
+++ src/components/header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Developer's Name</h1>
+      <nav>
+        <ul>
+          <li>About Me</li>
+          <li>Portfolio</li>
+          <li>Contact</li>
+          <li>Resume</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
