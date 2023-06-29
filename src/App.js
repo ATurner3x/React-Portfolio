18d44ae1f50a73ab79a281d6f4b6bf813f5bc4b8
Amadeus <a.turner3x@gmail.com>
2023-06-29 13:50:16 -0400

    update the app.js to work with custom components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
+import Header from './components/header';
+import Content from './components/content';
+import Footer from './components/footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Replace default header with custom Header component */}
+      <Header />
+
+      {/* Replace default content with custom Content component */}
+      <Content />
+
+      {/* Replace default footer with custom Footer component */}
+      <Footer />
     </div>
   );
 }
