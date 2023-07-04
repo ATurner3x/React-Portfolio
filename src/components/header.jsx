2260b7d32bfbe1f818363b721fc84ed58491b6b6
Amadeus <a.turner3x@gmail.com>
2023-07-04 06:15:13 -0400

    added icons to header buttons

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import '../styles/header.css';
+import { FaUser, FaBriefcase, FaEnvelope, FaFileAlt } from 'react-icons/fa';
 
 const Header = () => {
   return (
@@ -9,16 +10,24 @@ const Header = () => {
       <nav>
         <ul>
           <li>
-            <NavLink exact to="/" className="nav-button" activeClassName="active">About Me</NavLink>
+            <NavLink exact to="/" className="nav-button" activeClassName="active">
+              <FaUser /> About Me
+            </NavLink>
           </li>
           <li>
-            <NavLink to="/portfolio" className="nav-button" activeClassName="active">Portfolio</NavLink>
+            <NavLink to="/portfolio" className="nav-button" activeClassName="active">
+              <FaBriefcase /> Portfolio
+            </NavLink>
           </li>
           <li>
-            <NavLink to="/contact" className="nav-button" activeClassName="active">Contact</NavLink>
+            <NavLink to="/contact" className="nav-button" activeClassName="active">
+              <FaEnvelope /> Contact
+            </NavLink>
           </li>
           <li>
-            <NavLink to="/resume" className="nav-button" activeClassName="active">Resume</NavLink>
+            <NavLink to="/resume" className="nav-button" activeClassName="active">
+              <FaFileAlt /> Resume
+            </NavLink>
           </li>
         </ul>
       </nav>
@@ -27,4 +36,3 @@ const Header = () => {
 };
 
 export default Header;
-
