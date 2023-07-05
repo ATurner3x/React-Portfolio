66e96a5c0b6b0ebc0a124cb5f51ac07c7c8d6e14
Amadeus <a.turner3x@gmail.com>
2023-07-04 21:50:57 -0400

    updated footer component with icons and links

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
-import '../styles/footer.css'
+import { FaGithub, FaLinkedin, FaStackOverflow, FaTwitter } from 'react-icons/fa';
+import '../styles/footer.css';
+
 const Footer = () => {
   return (
     <footer>
-      <p>Text or icon links to GitHub, LinkedIn, and a third platform</p>
+      <p>Connect with me:</p>
+      <div className="social-links">
+        <a href="https://github.com/your-github-profile" target="_blank" rel="noopener noreferrer">
+          <FaGithub />
+        </a>
+        <a href="https://www.linkedin.com/in/your-linkedin-profile" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin />
+        </a>
+        <a href="https://stackoverflow.com/users/your-stackoverflow-profile" target="_blank" rel="noopener noreferrer">
+          <FaStackOverflow />
+        </a>
+        <a href="https://twitter.com/your-twitter-profile" target="_blank" rel="noopener noreferrer">
+          <FaTwitter />
+        </a>
+      </div>
     </footer>
   );
 };
